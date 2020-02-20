57d68941b7c1484a20b6eff48fc9560385a19e3f
iamkaith <klramirez665@gmail.com>
2020-02-20 00:43:28 -0800

    Add preliminary validation using data annotations. Require testing.

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -6,10 +6,10 @@ namespace Assignment2.Models {
     public class ApplicationUser : IdentityUser {
 
         public ApplicationUser() : base() {}
-
+        
         [Required]
         public string FirstName { get; set; }
-
+        
         [Required]
         public string LastName { get; set; }
         
@@ -22,18 +22,17 @@ namespace Assignment2.Models {
         [MaxLength(2)]
         public string Province { get; set; }
 
-        // Postal code regex: https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch04s15.html
-        [MaxLength(7), RegularExpression("^(?!.*[DFIOQU])[A-VXY][0-9][A-Z]●?[0-9][A-Z][0-9]$")]
+        // Postal code regex: https://blog.platformular.com/2012/03/03/how-to-validate-canada-postal-code-in-c/
+        [MaxLength(7), RegularExpression("^[ABCEGHJ-NPRSTVXY]{1}[0-9]{1}[ABCEGHJ-NPRSTV-Z]{1}[ ]?[0-9]{1}[ABCEGHJ-NPRSTV-Z]{1}[0-9]{1}$")]
         public string PostalCode { get; set; }
         
         public string Country { get; set; }
-
-        [Required]
+        
+        [Required, Range(-90.0, 90.0)]
         public double Latitude { get; set; }
-
-        [Required]
+        
+        [Required, Range(0.0, 180.0)]
         public double Longitude { get; set; }
-
         public bool IsNaugthy { get; set; }
     }
 }
--- ViewModels/Register.cs
+++ ViewModels/Register.cs
@@ -8,9 +8,9 @@ namespace Assignment2.ViewModels {
         public string Email { get; set; }
         [Required]
         public string Password { get; set; }
-
+        [Required]
         public string FirstName { get; set; }
-
+        [Required]
         public string LastName { get; set; }
         
         public string BirthDate { get; set; }
@@ -19,16 +19,18 @@ namespace Assignment2.ViewModels {
         
         public string City { get; set; }
 
+        [MaxLength(2)]
         public string Province { get; set; }
 
+        [MaxLength(7), RegularExpression("^[ABCEGHJ-NPRSTVXY]{1}[0-9]{1}[ABCEGHJ-NPRSTV-Z]{1}[ ]?[0-9]{1}[ABCEGHJ-NPRSTV-Z]{1}[0-9]{1}$")]
         public string PostalCode { get; set; }
         
         public string Country { get; set; }
 
-        [Required]
+        [Required, Range(-90.0, 90.0)]
         public double Latitude { get; set; }
 
-        [Required]
+        [Required, Range(0.0, 180.0)]
         public double Longitude { get; set; }
     }
 }
